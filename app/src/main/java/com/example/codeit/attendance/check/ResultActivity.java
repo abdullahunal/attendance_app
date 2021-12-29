82c73c04c0a9ebad35052ab145b244f22f488502
Moturcu66 <unalabdullahnuri@gmail.com>
2021-12-29 21:16:34 +0300

    Result Activity şablonu oluşturuldu

--- app/src/main/java/com/example/codeit/attendance/check/ResultActivity.java
+++ app/src/main/java/com/example/codeit/attendance/check/ResultActivity.java
@@ -1,9 +1,11 @@
 package com.example.codeit.attendance.check;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.List;
 
 // TEMSILI SINIF
-public class ResultActivity {
+public class ResultActivity extends AppCompatActivity {
 
     public void setResult(CheckResult checkResult) {
         // TODO: Dummy Method!
